5a9299b3e5eb31272ed8a60a5c0f9d7686eb4fef
Friedrich Delgado <delgado@dfn-cert.de>
2017-02-28 15:58:39 +0100

    Parametrisiere nach größe des Bildschirms

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ function feld20er (num1, num2) {
   // http://www.html5canvastutorials.com/tutorials/html5-canvas-circles/
   var canvas = document.createElement('canvas');
   canvas.id = 'ZahlenfelderPlot' + num1 + num2;
-  canvas.width = 120 * 21;
-  canvas.height = 120 * 2 ;
+  var space =  window.outerWidth / 21;
+  var step = space / 12;
+  var radius = step * 4;
+  var distance = step * 5;
+  canvas.width = window.outerWidth;
+  canvas.height = space * 2 ;
   document.querySelector('body').appendChild(canvas);
   // var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
 
   for (var i = 0; i < 20; i ++) {
-    var centerX =  50 + (i % 10) * 100;
+    var centerX =  distance + (i % 10) * space;
     if ((i % 10) > 4) {
-      centerX += 20;
+      centerX += step * 2;
     }
-    var centerY = 50;
+    var centerY = distance;
     if (i > 9) {
-      centerY += 110;
+      centerY += step * 11;
     }
-    var radius = 40;
 
     context.beginPath();
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
@@ -31,7 +34,7 @@ function feld20er (num1, num2) {
       context.fillStyle = colors[1];
       context.fill();
     }
-    context.lineWidth = 5;
+    context.lineWidth = step / 2;
     context.strokeStyle = '#003300';
     context.stroke();
   }
