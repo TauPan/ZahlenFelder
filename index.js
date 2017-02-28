3d054e7a6958dbaf3c9320c78a0181c8f387b2ad
Friedrich Delgado <delgado@dfn-cert.de>
2017-02-28 15:08:54 +0100

    schlimmen Denkfehler korrigiert: Radius 40 -> Durchmesser 80

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var canvas = document.createElement('canvas');
 canvas.id = 'ZahlenfelderPlot';
-canvas.width = 50 * 20;
-canvas.height = 50 * 2 ;
+canvas.width = 120 * 20;
+canvas.height = 100 * 2 ;
 document.querySelector('body').appendChild(canvas)
 // paste from http://www.html5canvastutorials.com/tutorials/html5-canvas-circles/
 // var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
 for (var i = 0; i < 20; i ++) {
-  var centerX = (5 + i) % 10  * 50;
-  var centerY = i > 9 ? 50 : 0;
+  var centerX =  (i % 10) * 100;
+  if ((i % 10) > 9) {
+    centerX += 10
+  }
+  var centerY = i > 9 ? 100 : 0;
   var radius = 40;
 
   context.beginPath();
