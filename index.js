58f43325e0510c50defe6e887cd259648924ed6f
Friedrich Delgado <delgado@dfn-cert.de>
2017-02-28 15:13:54 +0100

    Abstand zwischen 5er Gruppen korrigiert
    Divisionsrest von 10 soll größer als 9 sein? Ja sicher! ;)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var canvas = document.createElement('canvas');
 canvas.id = 'ZahlenfelderPlot';
-canvas.width = 120 * 20;
+canvas.width = 120 * 21;
 canvas.height = 120 * 2 ;
 document.querySelector('body').appendChild(canvas)
 // paste from http://www.html5canvastutorials.com/tutorials/html5-canvas-circles/
@@ -9,8 +9,8 @@ var context = canvas.getContext('2d');
 
 for (var i = 0; i < 20; i ++) {
   var centerX =  50 + (i % 10) * 100;
-  if ((i % 10) > 9) {
-    centerX += 10
+  if ((i % 10) > 4) {
+    centerX += 20
   }
   var centerY = 50;
   if (i > 9) {
